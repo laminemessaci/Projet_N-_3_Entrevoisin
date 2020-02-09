01c193b0a571450ffcc1e900d928aa84fa816479
messaci <lamine.messaci18@gmail.com>
2020-02-09 16:00:04 +0100

    Add FavoriteFragment and add item in ListNeighbourPagerAdapter 2 fragments

--- Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourPagerAdapter.java
+++ Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourPagerAdapter.java
@@ -18,7 +18,18 @@ public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
      */
     @Override
     public Fragment getItem(int position) {
-        return NeighbourFragment.newInstance();
+
+        switch (position){
+
+            case 0:
+                return NeighbourFragment.newInstance();
+            case 1:
+                return FavoriteFragment.newInstance();
+
+                default:
+                    return null;
+        }
+
     }
 
     /**
@@ -27,6 +38,6 @@ public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
      */
     @Override
     public int getCount() {
-        return 1;
+        return 2;
     }
 }
