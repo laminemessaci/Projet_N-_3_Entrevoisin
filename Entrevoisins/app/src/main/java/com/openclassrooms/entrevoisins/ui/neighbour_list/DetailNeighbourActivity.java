fac02787ff26079407f6e09186a8710a394daa7c
messaci <lamine.messaci18@gmail.com>
2020-02-09 15:36:11 +0100

    Add DetailNeighbourActivity fragement file source fragment_detail.xml

---
+++ Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/DetailNeighbourActivity.java
@@ -0,0 +1,37 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.openclassrooms.entrevoisins.R;
+
+import java.io.Serializable;
+
+public class DetailNeighbourActivity extends AppCompatActivity {
+
+    private DetailFragment mDetailFragment;
+    private Serializable mNeighbour;
+    public static String NEIGHBOUR = "NEIGHBOUR";
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_neighbour);
+
+        if (getIntent().hasExtra(NEIGHBOUR))
+        {
+            mNeighbour = getIntent().getSerializableExtra(NEIGHBOUR);
+        }
+
+        Bundle args = new Bundle();
+        args.putSerializable(NEIGHBOUR,mNeighbour);
+
+        mDetailFragment = new DetailFragment();
+        mDetailFragment.setArguments(args);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.detail_activity_framelayout,mDetailFragment)
+                .commit();
+
+    }
+}
