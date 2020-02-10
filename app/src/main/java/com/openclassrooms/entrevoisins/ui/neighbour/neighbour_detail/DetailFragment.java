4a4452883049230c595af2a083614d370b7d22af
messaci <lamine.messaci18@gmail.com>
2020-02-10 21:13:36 +0100

    Structur MVC and add add some packages Add Event to manage the click on itemDetailNeighbour

---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour/neighbour_detail/DetailFragment.java
@@ -0,0 +1,115 @@
+package com.openclassrooms.entrevoisins.ui.neighbour.neighbour_detail;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.di.DI;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+import com.openclassrooms.entrevoisins.service.NeighbourApiService;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ *
+ */
+public class DetailFragment extends Fragment {
+
+    @BindView(R.id.neigbourhNameTitle)
+    TextView neighbourNameTitle;
+    @BindView(R.id.neigbourName)
+    TextView neighbourName;
+    @BindView(R.id.pictureNeighbour)
+    ImageView neighbourPicture;
+    @BindView(R.id.tv_socialNetwork)
+    TextView social;
+    @BindView(R.id.fab_favorite)
+    FloatingActionButton fab_favorite;
+    @BindView(R.id.iv_back)
+    ImageView back;
+
+    private NeighbourApiService mApiService;
+    protected Neighbour mNeighbour;
+
+    public DetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle args = getArguments();
+        if (args.getSerializable(DetailNeighbourActivity.NEIGHBOUR) != null){
+            mNeighbour = (Neighbour) args.getSerializable(DetailNeighbourActivity.NEIGHBOUR);
+        }
+        mApiService = DI.getNeighbourApiService();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        ButterKnife.bind(this,view);
+
+        neighbourNameTitle.setText(mNeighbour.getName());
+        neighbourName.setText(mNeighbour.getName());
+        social.setText("www.facebook.fr/" + mNeighbour.getName());
+
+        Glide.with(this)
+                .load(mNeighbour.getAvatarUrl())
+                .centerCrop()
+                .into(neighbourPicture);
+
+        setFavoriteImage();
+
+        fab_favorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                setFabFavorite();
+            }
+        });
+
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+
+        return view;
+    }
+
+    private void setFavoriteImage() {
+        if (mApiService.isFavorite(mNeighbour)){
+            fab_favorite.setImageDrawable(getResources().getDrawable(R.drawable.ic_star_white_24dp));
+        }
+        else{
+            fab_favorite.setImageDrawable(getResources().getDrawable(R.drawable.ic_star_border_white_24dp));
+        }
+    }
+
+    public void setFabFavorite(){
+        if (mApiService.isFavorite(mNeighbour)){
+            mApiService.removeFavoriteNeighbour(mNeighbour);
+            Toast.makeText(this.getContext(),"Le voisin à été suprimé dans vos favoris", Toast.LENGTH_SHORT).show();
+            setFavoriteImage();
+        }
+        else{
+            mApiService.addNeighbourFavorite(mNeighbour);
+            Toast.makeText(this.getContext(),"Le voisin à été ajouté dans vos favoris", Toast.LENGTH_SHORT).show();
+            setFavoriteImage();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour/neighbour_detail/neighbour_favorite/FavoriteFragment.java
@@ -0,0 +1,99 @@
+package com.openclassrooms.entrevoisins.ui.neighbour.neighbour_detail.neighbour_favorite;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.openclassrooms.entrevoisins.R;
+import com.openclassrooms.entrevoisins.di.DI;
+import com.openclassrooms.entrevoisins.events.DeleteNeighbourEvent;
+import com.openclassrooms.entrevoisins.model.Neighbour;
+import com.openclassrooms.entrevoisins.service.NeighbourApiService;
+import com.openclassrooms.entrevoisins.ui.neighbour.neighbour_list.MyNeighbourRecyclerViewAdapter;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FavoriteFragment extends Fragment {
+
+    private NeighbourApiService mApiService;
+    private List<Neighbour> mNeighbours;
+    private RecyclerView mRecyclerView;
+
+    /**
+     * Create and return a new instance
+     * @return @{@link FavoriteFragment}
+     */
+    public static FavoriteFragment newInstance() {
+        FavoriteFragment fragment = new FavoriteFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mApiService = DI.getNeighbourApiService();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_neighbour_favorite_list, container, false);
+        Context context = view.getContext();
+        mRecyclerView = (RecyclerView) view;
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        initList();
+        return view;
+    }
+
+    /**
+     * Init the List of favorites neighbours
+     */
+    private void initList() {
+        mNeighbours = mApiService.getFavoriteNeighbour();
+        mRecyclerView.setAdapter(new MyNeighbourRecyclerViewAdapter (mNeighbours));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        initList();
+    }
+
+    /**
+     * Fired if the user clicks on a delete button
+     * @param event
+     */
+    @Subscribe
+    public void onDeleteNeighbourEvent(DeleteNeighbourEvent event) {
+        mApiService.deleteNeighbour(event.neighbour);
+        initList();
+    }
+}
+
+
